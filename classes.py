4532b0f0128b038cf3a745b0a923833165bb94fa
begduu <begduu0615@gmail.com>
2025-10-05 20:31:33 -0400

    fix small things

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 
 from helpers import load_card_data
+
 class Player:
     def __init__(self, deck, hand, faction, leader_card):
         self.deck = deck
@@ -7,7 +8,7 @@ class Player:
         self.discard_pile = []
         self.faction = faction
         self.leader_card = leader_card
-        self.hp = 2
+        self.num_wins = 0
 
 class Board:
     def __init__(self):
@@ -47,12 +48,29 @@ class Deck():
     def __init__(self):
         self.cards = []
 
-    def create_deck(self, master_card_map, deck_name, decks_data):
-        deck_ids = decks_data[deck_name]["card_ids"]
-        for card in master_card_map:
-            self.cards.append(master_card_map)
-   
+    def create_deck(self, master_card_map, faction, deck_name, decks_data):
+        target_deck_data = None
+        for deck_info in decks_data.get(faction, []):
+            if deck_info.get("name") == deck_name:
+                target_deck_data = deck_info
+                break
 
+        if target_deck_data:
+            card_ids = target_deck_data.get("card_ids", [])
+
+            for card_id in card_ids:
+                card = master_card_map.get(card_id)
+                if not card:
+                    continue
+                if card["type"] == "Weather":
+                    self.cards.append(WeatherCard(card["name"], card["ability"]))
+                elif card["type"] == "Troop":
+                    self.cards.append(TroopCard(card["name"], int(card["Strength"]), card["ability"]))
+                elif card["type"] == "Special":
+                    self.cards.append(SpecialCard(card["name"], card["ability"]))
+                else:
+                    continue
+       
 class GameEngine:
     def __init__(self):
         self.master_card_map = load_card_data("cards.json")
--- helpers.py
+++ helpers.py
@@ -2,11 +2,11 @@ import json
 
 def load_card_data(filepath: str) -> dict:
     """Loads the master card data from a JSON file into a dictionary."""
-    data_map = {}
+    data_dict = {}
     with open(filepath, "r") as file:
         for card in json.load(file):
-            data_map[card["id"]] = card
-    return data_map
+            data_dict[card["id"]] = card
+    return data_dict
 
 def load_deck_data(filepath: str) -> dict:
     """Loads pre-built deck configurations from a JSON file."""
